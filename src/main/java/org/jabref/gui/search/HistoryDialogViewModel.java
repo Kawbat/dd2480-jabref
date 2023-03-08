c2a3d0ec858cc864e7268f7312110fd2da26f734
Marcus Friberg <xolvez@gmail.com>
2023-03-08 14:57:46 +0100

    Issue #69: Remove the copy history button
    Removed the copy history button and removed unused fields and methods.

--- src/main/java/org/jabref/gui/search/HistoryDialogViewModel.java
+++ src/main/java/org/jabref/gui/search/HistoryDialogViewModel.java
@@ -1,25 +1,14 @@
 package org.jabref.gui.search;
 
-import java.util.Objects;
-
 import javafx.collections.ObservableList;
 
 import org.jabref.gui.AbstractViewModel;
-import org.jabref.gui.ClipBoardManager;
-import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
-import org.jabref.logic.l10n.Localization;
 
 public class HistoryDialogViewModel extends AbstractViewModel {
-
-    private final DialogService dialogService;
-    private final ClipBoardManager clipBoardManager;
-
     private final StateManager stateManager;
 
-    public HistoryDialogViewModel(DialogService dialogService, ClipBoardManager clipBoardManager, StateManager stateManager) {
-        this.dialogService = Objects.requireNonNull(dialogService);
-        this.clipBoardManager = Objects.requireNonNull(clipBoardManager);
+    public HistoryDialogViewModel(StateManager stateManager) {
         this.stateManager = stateManager;
     }
 
@@ -29,10 +18,5 @@ public class HistoryDialogViewModel extends AbstractViewModel {
 
     public void remove(String searchTerm) {
         this.stateManager.getSearchHistory().remove(searchTerm);
-    }
-
-    public void copyHistoryToClipboard() {
-        clipBoardManager.setContent("toCopy");
-        dialogService.notify(Localization.lang("Copied search histories to clipboard"));
     }
 }
